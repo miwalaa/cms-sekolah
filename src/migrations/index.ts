92c00766e58140281d4d3c62896f4abd972156a2
miwamiwamiwa <anonimdante79@gmail.com>
2025-10-17 04:23:15 +0700

    remove migration folder

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251004_204708 from './20251004_204708';
-
-export const migrations = [
-  {
-    up: migration_20251004_204708.up,
-    down: migration_20251004_204708.down,
-    name: '20251004_204708'
-  },
-];
